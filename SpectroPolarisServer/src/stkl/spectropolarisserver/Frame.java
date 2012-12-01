fc3270b2c5917c0a3bdcba4cbfe2ab67679e45a9
Klaas Winter <klaas@localhost.localdomain>
2012-12-01 15:28:47 +0100

    Deleted eclipse plugin

--- SpectroPolarisServer/src/stkl/spectropolarisserver/Frame.java
+++ SpectroPolarisServer/src/stkl/spectropolarisserver/Frame.java
@@ -1,21 +1,34 @@
-import ;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Frame extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5151041547543472432L;
+
 	public Frame() {
 		
 		initGUI();
 	}
-	
-	public initGUI() {
+
+	private void initGUI() {
 		setTitle("SpectroPolaris");
+		setUndecorated(true);
+		setResizable(false);
 		
-		setDefaultClosedOperation(JFrame.HIDE_ON_CLOSE);
+		
+		GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(this);
+		
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		
 		JButton hostGame = new JButton("Host Game");
 		
 		add(hostGame);
-		
+		validate();
 	}
 
 }

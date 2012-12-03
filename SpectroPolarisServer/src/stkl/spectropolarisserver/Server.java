c29c9f549126f94163084e5b5d61d30fc2cbe3a0
Klaas Winter <klaas@localhost.localdomain>
2012-12-03 12:01:46 +0100

    Added a Server

---
+++ SpectroPolarisServer/src/stkl/spectropolarisserver/Server.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Server extends Thread {
+	
+	private ServerSocket d_server;
+	
+	private ArrayList<ServerThread> d_threads;
+	
+	public Server(int port)
+	{
+		d_threads = new ArrayList<ServerThread>();
+		
+		d_server = null;
+		try {
+			d_server = new ServerSocket(port); 
+			System.out.println("Server up and running!");
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage() + ", java... :|");
+			System.exit(1);
+		}
+	}
+	
+    @Override
+    public void run() {
+    	while(true)
+    	{
+    		try {
+				Socket socket = d_server.accept();
+				ServerThread thread = new ServerThread(socket);
+				thread.start();
+				
+				d_threads.add(thread);
+			} catch (Exception e) {
+				System.out.println(e.getMessage() + ", java... :|");
+			}
+    	}
+    }
+    
+    public InetAddress ip() {
+    	return d_server.getInetAddress();
+    }
+	
+}

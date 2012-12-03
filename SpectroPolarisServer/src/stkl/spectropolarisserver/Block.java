453750ab60164cb8a5ac13af8aa1e881171d6421
Klaas Winter <klaas@localhost.localdomain>
2012-12-03 20:42:16 +0100

    Added block class

---
+++ SpectroPolarisServer/src/stkl/spectropolarisserver/Block.java
@@ -0,0 +1,55 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+
+public class Block {
+	private int d_x;
+	private int d_y;
+	
+	private int d_width;
+	private int d_height;
+	
+	public Block(int x, int y, int width, int height) {
+		d_x = x;
+		d_y = y;
+		d_width = width;
+		d_height = height;
+	}
+	
+	public void update(int x, int y, int width, int height) {
+		d_x = x;
+		d_y = y;
+		d_width = width;
+		d_height = height;
+	}
+	
+	public int x() {
+		return d_x;
+	}
+	
+	public int y() {
+		return d_y;
+	}
+	
+	public int width() {
+		return d_width;
+	}
+	
+	public int height() {
+		return d_height;
+	}
+	
+    public boolean pointCollision(int x, int y) {
+    	return x > d_x && y > d_y && x < d_x + d_width && y < d_y + d_height;
+    }
+
+	public void draw(Graphics2D g2d) {
+
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+			RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		g2d.setColor(Color.black);
+		g2d.fillRect(d_x, d_y, d_width, d_height);
+	}
+}

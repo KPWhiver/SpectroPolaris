c3ce83acfdcf564d5ae0bf0edcfc14f12c456adb
Sab Eros <mini.game@hotmail.com>
2012-12-04 18:56:22 +0100

    Added music to main menu activity

--- SpectroPolarisClient/src/stkl/spectropolarisclient/MenuActivity.java
+++ SpectroPolarisClient/src/stkl/spectropolarisclient/MenuActivity.java
@@ -2,6 +2,7 @@ package stkl.spectropolarisclient;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -9,13 +10,25 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class MenuActivity extends Activity {
+	MediaPlayer mediaPlayer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+        
         setContentView(R.layout.activity_menu);
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	mediaPlayer.start();
     }
 
     @Override
@@ -28,6 +41,7 @@ public class MenuActivity extends Activity {
      * Join game button has been pressed. Start the JoinActivity.
      */
     public void joinGame(View view) {
+    	mediaPlayer.pause();
     	Intent myIntent = new Intent(MenuActivity.this, JoinActivity.class);
     	MenuActivity.this.startActivity(myIntent);
     }
@@ -36,6 +50,8 @@ public class MenuActivity extends Activity {
      * Quit button has been pressed. Stop the activity, closing the application.
      */
     public void quit(View view) {
+    	mediaPlayer.stop();
+    	mediaPlayer.release();
     	finish();
     }
     

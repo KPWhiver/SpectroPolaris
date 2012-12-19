9f449316180032f695d4ae44b84f68ca65c46bc2
s2036851 <s2036851@wing.rug.nl>
2012-12-19 11:48:45 +0100

    Fixed enemies hugging the players

--- SpectroPolarisServer/src/stkl/spectropolarisserver/Enemy.java
+++ SpectroPolarisServer/src/stkl/spectropolarisserver/Enemy.java
@@ -7,7 +7,6 @@ public class Enemy extends GameCharacter {
 	private Stack<Node> path;
 	private Node goal;
 	private int lastPlayerPath;
-	private int lastShotFired;
 	private long d_timeSinceLastBullet = 0;
 
 	public Enemy(float x, float y, Color color) {
@@ -16,7 +15,6 @@ public class Enemy extends GameCharacter {
 		path = model.findPath(d_x, d_y, model.hill().x, model.hill().y);
 		goal = path.pop();
 		lastPlayerPath = 0;
-		lastShotFired = 0;
 	}
 
 	
@@ -26,14 +24,19 @@ public class Enemy extends GameCharacter {
 		d_speed = 1;
 		
 		Player player = model.closestPlayer(d_x, d_y, 100);
-		if(player != null && lastPlayerPath < 0) {
-			if(System.nanoTime() - d_timeSinceLastBullet > 250000000 && player.distanceFrom(d_x, d_y) < 80 && model.visible(d_x, d_y, player.d_x, player.d_y) == null) {
+		if(player != null && lastPlayerPath < 0) {			
+			
+			if(player.distanceFrom(d_x, d_y) < 80 && model.visible(d_x, d_y, player.d_x, player.d_y) == null) {				
+				if(System.nanoTime() - d_timeSinceLastBullet > 250000000) {
+					model.addBullet().instantiate(d_x, d_y, (float) Math.atan2(player.d_x - d_x, player.d_y - d_y), id());
+					d_timeSinceLastBullet = System.nanoTime();
+				}
+
 				d_speed = 0;
-				model.addBullet().instantiate(d_x, d_y, (float) Math.atan2(player.d_x - d_x, player.d_y - d_y), id());
-				d_timeSinceLastBullet = System.nanoTime();
 				return;
-			} 
-			lastShotFired++;
+			}
+			
+
 			
 			path = model.findPath(d_x, d_y, player.x(), player.y());
 			path.pop();

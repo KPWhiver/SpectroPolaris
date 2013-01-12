0f2a5cd5ca505f92111a43e5799ddb54dd91da9f
Sab Eros <mini.game@hotmail.com>
2013-01-12 19:01:20 +0100

    Changed volume button event handling

--- SpectroPolarisClient/src/stkl/spectropolarisclient/GameActivity.java
+++ SpectroPolarisClient/src/stkl/spectropolarisclient/GameActivity.java
@@ -74,17 +74,23 @@ public class GameActivity extends Activity {
     }
     
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
-            d_model.player().changeWeapon(-1);
-            return true;
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            	if (action == KeyEvent.ACTION_DOWN) {
+            	d_model.player().changeWeapon(-1);
+            	}
+            	return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                	d_model.player().changeWeapon(1);
+                }
+                return true;
+            default:
+                return super.dispatchKeyEvent(event);
         }
-        else if(keyCode == KeyEvent.KEYCODE_VOLUME_UP){
-        	d_model.player().changeWeapon(1);
-            return true;
-        }
-
-        return super.onKeyDown(keyCode, event);
     }
 
     
